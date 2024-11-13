876d208d4d8266c2112e1f15c7275e137e3ef9f7
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2024-11-13 09:59:36 +0000

    refactor: Add generator node base (#2006)

--- src/generator/addPair.h
+++ src/generator/addPair.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "base/units.h"
+#include "generator/addBase.h"
 #include "generator/node.h"
 #include "generator/nodeValue.h"
 
@@ -12,7 +13,7 @@ class Species;
 class RegionGeneratorNodeBase;
 
 // AddPair Node
-class AddPairGeneratorNode : public GeneratorNode
+class AddPairGeneratorNode : public AddGeneratorNodeBase
 {
     public:
     explicit AddPairGeneratorNode(const Species *spA = nullptr, const Species *spB = nullptr, const NodeValue &population = 0,
@@ -25,54 +26,15 @@ class AddPairGeneratorNode : public GeneratorNode
     void setUpKeywords();
 
     /*
-     * Identity
-     */
-    public:
-    // Return whether a name for the node must be provided
-    bool mustBeNamed() const override;
-
-    /*
      * Node Data
      */
-    public:
-    // Box Action Style
-    enum class BoxActionStyle
-    {
-        None,        /* Box geometry / volume will remain unchanged */
-        AddVolume,   /* Increase Box volume to accommodate new species, according to supplied density */
-        ScaleVolume, /* Scale current Box volume to give, after addition of the current species, the supplied density */
-        Set          /* Set the Box geometry to that specified in the Species */
-    };
-    // Return enum option info for BoxActionStyle
-    static EnumOptions<BoxActionStyle> boxActionStyles();
-    // Positioning Type
-    enum class PositioningType
-    {
-        Central, /* Position the Species at the centre of the Box */
-        Current, /* Use current Species coordinates */
-        Random,  /* Set position randomly */
-        Region   /* Set position randomly within a specified region */
-    };
-    // Return enum option info for PositioningType
-    static EnumOptions<PositioningType> positioningTypes();
-
     private:
-    // Action to take on the Box geometry / volume on addition of the species
-    AddPairGeneratorNode::BoxActionStyle boxAction_{AddPairGeneratorNode::BoxActionStyle::AddVolume};
-    // Target density when adding molecules
-    std::pair<NodeValue, Units::DensityUnits> density_{1.0, Units::GramsPerCentimetreCubedUnits};
-    // Population of molecules to add
-    NodeValue population_{1.0};
-    // Positioning type for individual molecules
-    AddPairGeneratorNode::PositioningType positioningType_{AddPairGeneratorNode::PositioningType::Random};
-    // Region into which we will add molecules (if any)
-    std::shared_ptr<const RegionGeneratorNodeBase> region_{nullptr};
+    // Species to be added
+    const Species *speciesA_{nullptr}, *speciesB_{nullptr};
     // Whether to rotate molecules on insertion
     bool rotate_{true};
     // Flags controlling box axis scaling
     bool scaleA_{true}, scaleB_{true}, scaleC_{true};
-    // Species to be added
-    const Species *speciesA_{nullptr}, *speciesB_{nullptr};
 
     /*
      * Execute
