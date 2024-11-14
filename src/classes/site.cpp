2fbdfd12273f65d0283664ae80fb382cc310baa8
Tristan Youngs <tristan.youngs@stfc.ac.uk>
2024-11-14 13:01:51 +0000

    refactor: Single site class (#2009)

--- src/classes/site.cpp
+++ src/classes/site.cpp
@@ -6,17 +6,23 @@
 #include "base/messenger.h"
 #include <utility>
 
-/*
- * Site
- */
+Site::Site(const SpeciesSite *parent, std::optional<int> uniqueSiteIndex, std::shared_ptr<const Molecule> molecule,
+           const Vec3<double> &origin)
+    : parent_(parent), uniqueSiteIndex_(uniqueSiteIndex), molecule_(std::move(molecule)), origin_(origin)
+{
+}
 
 Site::Site(const SpeciesSite *parent, std::optional<int> uniqueSiteIndex, std::shared_ptr<const Molecule> molecule,
-           Vec3<double> origin)
+           const Matrix3 &axes, const Vec3<double> &origin)
+    : parent_(parent), uniqueSiteIndex_(uniqueSiteIndex), molecule_(std::move(molecule)), origin_(origin), axes_(axes),
+      hasAxes_(true)
 {
-    parent_ = parent;
-    uniqueSiteIndex_ = uniqueSiteIndex;
-    molecule_ = std::move(molecule);
-    origin_ = origin;
+}
+
+// Return enum options for SiteAxis
+EnumOptions<Site::SiteAxis> Site::siteAxis()
+{
+    return EnumOptions<Site::SiteAxis>("SiteAxis", {{Site::XAxis, "X"}, {Site::YAxis, "Y"}, {Site::ZAxis, "Z"}});
 }
 
 // Return the parent
@@ -32,39 +38,7 @@ const Vec3<double> &Site::origin() const { return origin_; }
 std::shared_ptr<const Molecule> Site::molecule() const { return molecule_; }
 
 // Return whether local axes are present
-bool Site::hasAxes() const { return false; }
+bool Site::hasAxes() const { return hasAxes_; }
 
 // Return local axes
-const Matrix3 &Site::axes() const
-{
-    static Matrix3 dummy;
-    Messenger::warn("Returning empty axes for this Site, since it doesn't have any.\n");
-    return dummy;
-}
-
-/*
- * Oriented Site
- */
-
-OrientedSite::OrientedSite(const SpeciesSite *parent, std::optional<int> uniqueSiteIndex,
-                           std::shared_ptr<const Molecule> molecule, Vec3<double> origin, Vec3<double> xAxis,
-                           Vec3<double> yAxis, Vec3<double> zAxis)
-    : Site(parent, uniqueSiteIndex, std::move(molecule), origin)
-{
-    axes_.setColumn(0, xAxis);
-    axes_.setColumn(1, yAxis);
-    axes_.setColumn(2, zAxis);
-}
-
-// Return whether local axes are present
-bool OrientedSite::hasAxes() const { return true; }
-
-// Return enum options for SiteAxis
-EnumOptions<OrientedSite::SiteAxis> OrientedSite::siteAxis()
-{
-    return EnumOptions<OrientedSite::SiteAxis>(
-        "SiteAxis", {{OrientedSite::XAxis, "X"}, {OrientedSite::YAxis, "Y"}, {OrientedSite::ZAxis, "Z"}});
-}
-
-// Return local axes
-const Matrix3 &OrientedSite::axes() const { return axes_; }
+const Matrix3 &Site::axes() const { return axes_; }
